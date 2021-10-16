3bce014c64459309a6d9315ac5918bbefc8b2269
santos491 <91709423+santos491@users.noreply.github.com>
2021-10-15 23:14:50 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+//Nombre de la escuela => Universidad Tecnologica Metropolitana
+// Asignatura => Aplicaciones Web Orientadas a Servicios
+// Nombre del Maestro => Ing. Joel I. Chuc Uc
+// Nombre de la actividad => Actividad 1 (Unidad II) (Trabajo individual)
+// Nombre del alumno o Alumnos => SANTOS ANTONIO PECH SANTIAGO
+// Cuatrimestre =>4
+// Grupo => 4B
+// Parcial => SEGUNDO PARCIAL
+
 namespace QueryApi
 {
     public class Program
